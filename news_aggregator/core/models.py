12a0be359598d0328a8c551e3b9b0428036f62f3
ushankax <ushanka.x@gmail.com>
2020-09-18 16:59:21 +0300

    'models.py' optimisation

--- news_aggregator/core/models.py
+++ news_aggregator/core/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Article(models.Model):
     title = models.CharField(max_length=255)
-    link = models.CharField(max_length=55)
+    link = models.URLField(max_length=100)
     text = models.TextField()
 
     def __str__(self):
