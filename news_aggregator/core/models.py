0654d5c3aee01fbdfce62ce9d54920ab17753649
ushankax <ushanka.x@gmail.com>
2020-09-18 21:40:07 +0300

    extended 'Article' model (text_preview field)

--- news_aggregator/core/models.py
+++ news_aggregator/core/models.py
@@ -5,6 +5,7 @@ class Article(models.Model):
     title = models.CharField(max_length=255)
     link = models.URLField(max_length=100)
     text = models.TextField()
+    text_preview = models.TextField()
 
     def __str__(self):
         return self.title
