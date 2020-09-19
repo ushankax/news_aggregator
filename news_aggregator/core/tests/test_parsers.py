1a7d65b9e9707beaf905fae1ab4f922598d64099
ushankax <ushanka.x@gmail.com>
2020-09-19 14:34:05 +0300

    some tests for VCParser

--- news_aggregator/core/tests/test_parsers.py
+++ news_aggregator/core/tests/test_parsers.py
@@ -1,11 +1,14 @@
 from django.test import TestCase
-from core.parsers import SiteParser
+from core.parsers import SiteParser, VCParser
 
 
-class HabrParserTest(TestCase):
+class SiteParserTest(TestCase):
+    """This TestCase is also work to HabrParser for its similarity"""
 
     def setUp(self):
-         self.p = SiteParser('https://habr.com/ru/users/ushankax/favorites/', 'post__title_link')
+         self.p = SiteParser('https://habr.com/ru/users/ushankax/favorites/',
+                             'post__title_link',
+                             'post__body post__body_full')
 
     def test_get_urls(self):
         self.assertEqual(len(self.p.get_urls()), 41)
@@ -13,7 +16,21 @@ class HabrParserTest(TestCase):
     def test_get_title_and_text(self):
         article = self.p.get_urls()[0]
         title, text = self.p.get_title_and_text(article)
-        self.assertEqual(title, '\n[Разбор] Инвестиции и спекуляции: в чем на самом деле разница\n')
+        self.assertEqual(title, '[Разбор] Инвестиции и спекуляции: в чем на самом деле разница')
         self.assertEqual(text[:47], '\nВ нашем блоге мы много пишем о работе на бирже')
 
+
+class VCParserTest(TestCase):
+
+    def setUp(self):
+        self.p = VCParser()
+        self.article = 'https://vc.ru/transport/159612-avtopilot-tesla-sbezhal-ot-policii-v-kanade-voditel-usnul-i-mashina-uhodila-ot-pogoni-sama'
+
+    def test_get_urls(self):
+        self.assertEqual(len(self.p.get_urls()), 12)
+
+    def test_get_title_and_text(self):
+        title, text = self.p.get_title_and_text(self.article)
+        self.assertEqual(title, 'Автопилот Tesla сбежал от полиции в Канаде: водитель уснул и машина уходила от погони сама')
+        self.assertEqual(text[:25], '\nВладельца машины обвинил')
 
