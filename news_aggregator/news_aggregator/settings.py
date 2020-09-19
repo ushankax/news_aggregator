ef764236f8da2dc6850004b54f68706dbac4a3a5
ushankax <ushanka.x@gmail.com>
2020-09-19 16:31:28 +0300

    changed database from sqlite to postgresql

--- news_aggregator/news_aggregator/settings.py
+++ news_aggregator/news_aggregator/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'core',
     'rest_framework',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -77,8 +78,12 @@ WSGI_APPLICATION = 'news_aggregator.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'news_aggregator',
+        'USER': 'admin',
+        'PASSWORD': 'adminadmin',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
