bf52f35be2041093e84c20aefa2fed430b509462
ushankax <ushanka.x@gmail.com>
2020-09-19 15:45:09 +0300

    extended 'Article' model ('source' field)

--- news_aggregator/core/models.py
+++ news_aggregator/core/models.py
@@ -1,12 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Article(models.Model):
+    source = models.CharField(max_length=55)
     title = models.CharField(max_length=255)
     link = models.URLField(max_length=100)
     text = models.TextField()
     text_preview = models.TextField()
 
     def __str__(self):
-        return self.title
+        return "'{}' from {}".format(self.title, self.source)
 
