27912f5c12133a188f793b7f3e72ae724befa35e
ushankax <ushanka.x@gmail.com>
2020-09-20 20:31:05 +0300

    core/tasks.py: added task for uploading articles in database

--- news_aggregator/core/tasks.py
+++ news_aggregator/core/tasks.py
@@ -1,15 +1,36 @@
 from news_aggregator.celery import app
-from .models import Article
 import json
+from . import parsers
+from .models import Article
 
 
 @app.task()
-def test():
-    Article.objects.create(source='source',
-                                     title='title',
-                                     link='https://vc.ru/',
-                                     text='text',
-                                     text_preview='preview',
-                                    )
+def import_latest_news():
+    """upload unique articles from sites in database"""
+    vc = parsers.VCParser()
+    vc_links = vc.get_urls()
+
+    for link in vc_links:
+        if Article.objects.filter(link=link):
+            pass
+        else:
+            title_and_text = vc.get_title_and_text(link)
+            title, text = title_and_text
+
+            article = Article.objects.create(source='vc', title=title, link=link, text=text, text_preview=text[:700])
+            print(article)
+
+    habr = parsers.HabrParser()
+    habr_links = habr.get_urls()
+
+    for link in habr_links:
+        if Article.objects.filter(link=link):
+            pass
+        else:
+            title_and_text = habr.get_title_and_text(link)
+            title, text = title_and_text
+
+            article = Article.objects.create(source='habr', title=title, link=link, text=text, text_preview=text[:700])
+            print(article)
 
     return json.dumps({"status": True})
