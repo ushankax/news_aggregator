9c75c32c065c2a777b4e179bdbbbe92d1fc10179
ushankax <ushanka.x@gmail.com>
2020-09-20 19:19:14 +0300

    core/models.py: created 'import_date' field

--- news_aggregator/core/models.py
+++ news_aggregator/core/models.py
@@ -1,13 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 
 class Article(models.Model):
     source = models.CharField(max_length=55)
     title = models.CharField(max_length=255)
-    link = models.URLField(max_length=100)
+    link = models.URLField(max_length=255)
     text = models.TextField()
     text_preview = models.TextField()
+    import_date = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return "'{}' from {}".format(self.title, self.source)
