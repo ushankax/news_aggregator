e99b1c00f47a185ab2704221fcdceab27dc34690
ushankax <ushanka.x@gmail.com>
2020-09-21 18:20:36 +0300

    accounts app folder with tests

---
+++ news_aggregator/accounts/tests/test_serializers.py
@@ -0,0 +1,45 @@
+from django.test import TestCase, Client
+from accounts.serializers import UserSerializer
+from django.contrib.auth.models import User
+from django.contrib import auth
+from accounts.models import Profile
+
+
+class UserSerializerTest(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+
+        self.serializer_data = {
+            'username': 'new_user',
+            'password': 'password',
+            'subscriptions': ['habr']
+        }
+
+        self.serializer_patch_data = {
+            'username': 'new_user777',
+            'subscriptions': ['vc']
+        }
+
+        self.user = User.objects.create_user(username='patch_user', password='password')
+        self.profile = Profile.objects.create(user=self.user, subscriptions=['habr'])
+        self.user.save()
+        self.profile.save()
+
+    def test_user_profile_was_created(self):
+        resp = self.client.post('/users/', self.serializer_data)
+        self.assertEqual(resp.status_code, 201)
+
+        self.client.login(username='new_user', password='password')
+        user = auth.get_user(self.client)
+        self.assertTrue(user.is_authenticated)
+        self.assertEqual(user.profile.subscriptions, ['habr'])
+
+    def test_user_can_update_your_profile(self):
+        self.client.login(username='patch_user', password='password')
+        resp = self.client.patch('/users/1/', self.serializer_patch_data, content_type='application/json')
+        print(resp.status_code)
+
+        user = auth.get_user(self.client)
+
+        self.assertEqual(user.username, 'new_user777')
---
+++ news_aggregator/accounts/tests/test_views.py
@@ -0,0 +1,68 @@
+from django.test import TestCase, Client
+from accounts.views import UserViewSet
+from django.contrib.auth.models import User
+from accounts.models import Profile
+
+
+class UserViewSetTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        for n in range(3):
+            user = User.objects.create_user(username='user{}'.format(n),
+                                            password='password'
+                                           )
+            user.save()
+
+        superuser = User.objects.create_superuser(username='superuser',
+                                                  password='password',
+                                                 )
+        superuser.save()
+
+    def setUp(self):
+        self.client = Client()
+
+    def test_users_url_exists(self):
+        resp = self.client.get('/users/')
+        self.assertEqual(resp.status_code, 200)
+
+    def test_unlogged_users_cannot_see_users(self):
+        resp = self.client.get('/users/')
+        self.assertEqual(resp.json(), [])
+
+    def test_logged_users_see_only_themselves(self):
+        self.client.login(username='user1', password='password')
+        resp = self.client.get('/users/')
+        user_data = resp.json()[0]
+        self.assertEqual(len(resp.json()), 1)
+        self.assertEqual(user_data['username'], 'user1')
+
+    def test_superuser_sees_all_users(self):
+        self.client.login(username='superuser', password='password')
+        resp = self.client.get('/users/')
+        self.assertEqual(len(resp.json()), 4)
+
+    def test_unlogged_users_cant_see_detail_pages(self):
+        resp = self.client.get('/users/3/')
+        response = resp.json()
+        self.assertEqual(response['status'], 'request was permitted')
+
+    def test_logged_users_can_see_them_detail_pages(self):
+        self.client.login(username='user1', password='password')
+        resp = self.client.get('/users/5/')
+        user_data = resp.json()
+        self.assertEqual(len(user_data), 3)
+        self.assertEqual(user_data["username"], 'user1')
+
+    def test_logged_users_cannot_see_another_detail_pages(self):
+        user = self.client.login(username='user1', password='password')
+        resp = self.client.get('/users/6/')
+        response = resp.json()
+        self.assertEqual(response['status'], 'request was permitted')
+
+    def test_superuser_can_see_all_detail_pages(self):
+        superuser = self.client.login(username='superuser', password='password')
+        resp = self.client.get('/users/5/')
+        user_data = resp.json()
+        self.assertEqual(len(user_data), 3)
+
