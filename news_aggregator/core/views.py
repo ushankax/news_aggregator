10fee74c9ed43396035f65430e51c0c1be6511b9
ushankax <ushanka.x@gmail.com>
2020-09-21 11:21:27 +0300

    small fixes

--- news_aggregator/core/views.py
+++ news_aggregator/core/views.py
@@ -8,14 +8,13 @@ class ArticleViewSet(viewsets.ModelViewSet):
     """list of news by subscription for authorised users only"""
     permission_classes = [permissions.IsAuthenticated]
 
-    def get_serializer_class(self):
-        """news with preview text when listing"""
-        if self.action == 'list':
-            return ArticleListSerializer
-        return ArticleSerializer
-
     def get_queryset(self):
         """filter news by current user subscriptions"""
         subs = self.request.user.profile.subscriptions
         return Article.objects.filter(source__in=subs).order_by('-import_date')
 
+    def get_serializer_class(self):
+        """news with preview text when listing"""
+        if self.action == 'list':
+            return ArticleListSerializer
+        return ArticleSerializer
